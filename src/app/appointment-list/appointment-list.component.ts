c80c881aa4089afce1aec52b9537f0a5add82641
sidchopp <sidchopp@gmail.com>
2024-04-19 16:53:42 -0600

    created 1st interface Appointment and 1 way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
 export class AppointmentListComponent {
-
+  appointment: Appointment = {
+    id: 1,
+    title: 'Go for a walk',
+    date: new Date('2024-04-19'),
+  };
 }
